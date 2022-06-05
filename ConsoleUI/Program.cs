6fd8d69ee661a41654c68903487cab2dd0f89a20
aaronmdenny <aaronmdenny@outlook.com>
2022-06-05 11:12:16 -0400

    LSP definition

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,6 +7,14 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            /*
+             * Liskov Substitution Principle:
+             * If S is a subtype of T, then objects of type T may be replaced with objects of type S.
+             * 
+             * In this example, we should be able to replace the emp variable with an object of type Manager without
+             * breaking the program.
+             */
+
             Manager accountingVP = new Manager();
 
             accountingVP.FirstName = "Emma";
